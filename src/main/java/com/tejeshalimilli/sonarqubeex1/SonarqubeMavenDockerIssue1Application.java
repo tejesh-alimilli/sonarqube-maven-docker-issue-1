43b8146fdf51e35534d52ca8f6aa0953a7ed1f86
Alimilli Tejesh <tejesh-alimilli@users.noreply.github.com>
2023-03-09 18:36:37 +0800

    imported code from spring starter io

---
+++ src/main/java/com/tejeshalimilli/sonarqubeex1/SonarqubeMavenDockerIssue1Application.java
@@ -0,0 +1,13 @@
+package com.tejeshalimilli.sonarqubeex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeMavenDockerIssue1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeMavenDockerIssue1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejeshalimilli/sonarqubeex1/SonarqubeMavenDockerIssue1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejeshalimilli.sonarqubeex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeMavenDockerIssue1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
